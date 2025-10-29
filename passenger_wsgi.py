bb44e35f4a42744f43b578130d714ad76d681a63
kalabkassa <kalabkassa@gmail.com>
2025-10-29 13:22:53 +0300

    wsgi integration update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,12 @@
 import sys, os
 
 # Path to your project
-project_home = '/home/aditacoq/myproject'
+project_home = '/home/aditacoq/aidta-backend'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'adita_backend.settings'
+os.environ['PRODUCTION'] = '1'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

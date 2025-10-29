384147c697d73e3a52befbae117c3c531de10bb8
kalabkassa <kalabkassa@gmail.com>
2025-10-29 16:44:08 +0300

    email conf update

--- adita_backend/settings.py
+++ adita_backend/settings.py
@@ -132,7 +132,8 @@ if os.getenv("PRODUCTION"):
     EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL")
     EMAIL_HOST_USER = env("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-    DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+    DEFAULT_FROM_EMAIL="Adita Academy <noreply@aditacademy.co>"
+
 else:
     # Development: SQLite
     DATABASES = {

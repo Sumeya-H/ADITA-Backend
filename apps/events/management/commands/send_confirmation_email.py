6163bac9b67668eb23f464f0850f3043ab30e554
kalabkassa <kalabkassa@gmail.com>
2026-04-14 16:20:45 +0300

    email tested and set to be sent to all registerants

--- apps/events/management/commands/send_confirmation_email.py
+++ apps/events/management/commands/send_confirmation_email.py
@@ -10,17 +10,18 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         users = EventRegistration.objects.all()
 
-        # for user in users:
-        #    registrant_name = user.full_name
-        #    self.stdout.write(
-        #        f"Sending email to {registrant_name} ({user.email})...")
-        #    send_course_confirmation_email(
-        #        None, registrant_name, user.email, user.id)
-        #    self.stdout.write(self.style.SUCCESS(
-        #        f"Successfully sent email to {registrant_name}"))
-        self.stdout.write(
-            f"Sending email to Kalab Kassa kalabkassa@gmail.com ...")
-        send_course_confirmation_email(
-            None, "Dr. Sultan Feisso", "sultanmeko2@gmail.com", "1389076f-2696-43cd-8b63-e98342b78275")
-        self.stdout.write(self.style.SUCCESS(
-            f"Successfully sent email to kalab kassa"))
+        for user in users:
+           registrant_name = user.full_name
+           self.stdout.write(
+               f"Sending email to {registrant_name} ({user.email})...")
+           send_course_confirmation_email(
+               None, registrant_name, user.email, user.id)
+           self.stdout.write(self.style.SUCCESS(
+               f"Successfully sent email to {registrant_name}"))
+
+        # self.stdout.write(
+        #    f"Sending email to Kalab Kassa kalabkassa@gmail.com ...")
+        # send_course_confirmation_email(
+        #    None, "Dr. Sultan Feisso", "sultanmeko2@gmail.com", "1389076f-2696-43cd-8b63-e98342b78275")
+        # self.stdout.write(self.style.SUCCESS(
+        #    f"Successfully sent email to kalab kassa"))
